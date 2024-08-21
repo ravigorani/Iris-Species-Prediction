05180844e2157fee5cd2544d452347ec467d992a
ravigorani <goraniravikumar2003@gmail.com>
2024-08-21 08:39:24 +0530

    readme file update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Machine Learning Project Deployement
+Machine Learning Project
+
+This Model uses Logistic Regression for predicting the species.
+
+Libraries to be installed:
+    numpy
+    pandas
+    sklearn
+    pickle
+    flask
+
+Steps to run the App:
+    1. Open this folder in Terminal
+    2. Type "python app.py" and run the file
--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     # output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Predicted Iris Species is {}'.format(prediction))
+    return render_template('index.html', prediction_text='Predicted Iris Species is {}'.format(prediction[0]))
 
 if __name__ == "__main__":
     app.run(debug=True)
